7368a8c20a16e7c04c2e74288da659ced79488d5
MiterX <you@example.com>
2018-03-04 16:24:53 +0100

    make it better

---
+++ Key_secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         'RxvumjD5gacysKJSdBFugGYgH',
+  consumer_secret:      'O4QCTox3trfgl307eZhAnVCstQu3d6X2ZJ6KIYIRO6c6em9PSW',
+  access_token:         '924285815513022464-ci4j5xcYYtJb8yc7WbTUhxKLyqo0EIc',
+  access_token_secret:  '5vcqk0hnlz6z8PZKgYLdLephhIPEYMZIroHvjr9Picor4',
+  // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
+
